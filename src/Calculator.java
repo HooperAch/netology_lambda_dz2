d614e088a0b8cc994c0ffd687570acef67aff3e9
Filyuzin <forgogu@inbox.ru>
2023-08-27 01:48:21 +0700

    Test 3

--- src/Calculator.java
+++ src/Calculator.java
@@ -13,7 +13,7 @@ public class Calculator {
     BinaryOperator<Integer> plus = (x, y) -> x + y;
     BinaryOperator<Integer> minus = (x, y) -> x - y;
     BinaryOperator<Integer> multiply = (x, y) -> x * y;
-    BinaryOperator<Integer> devide = (x, y) ->  y != 0  ? x/ y : 0; //Деление на 0 пропускается. Требуется отдельно обрабатывать -> При делении на ноль 0 возвращаем 0
+    BinaryOperator<Integer> devide = (x, y) ->  y != 0  ? x/ y : 0; //git Деление на 0 пропускается. Требуется отдельно обрабатывать -> При делении на ноль 0 возвращаем 0
     UnaryOperator<Integer> pow = x -> x * x;
     UnaryOperator<Integer> abs = x -> x > 0 ? x : x * -1;
     Predicate<Integer> isPositive = x -> x > 0;
