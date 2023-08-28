80682543affce3278731c3588578008381dfda2e
filyuzin <forgogu@inbox.ru>
2023-08-28 22:32:25 +0700

    Fixed method

--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,10 @@ public class Main {
     public static void main(String[] args) {
 
         OnTaskDoneListener listener = System.out::println;
+        OnTaskErrorListener listenerEr = System.out::println;
 
-        Worker worker = new Worker(listener);
+
+        Worker worker = new Worker(listener,listenerEr);
         worker.start();
 
     }
--- src/Worker.java
+++ src/Worker.java
@@ -4,12 +4,15 @@ public class Worker {
     private OnTaskDoneListener callback;
     private OnTaskErrorListener errorCallback;
 
-    public Worker(OnTaskDoneListener callback) { this.callback = callback;    }
+    /*public Worker(OnTaskDoneListener callback) { this.callback = callback;    }
 
     public Worker(OnTaskErrorListener errorCallback) {
         this.errorCallback = errorCallback;
+    }*/
+    public Worker(OnTaskDoneListener callback, OnTaskErrorListener errorCallback) {
+        this.callback = callback;
+        this.errorCallback = errorCallback;
     }
-
 
 
     public void start() {
